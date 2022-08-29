52d3265afd88be619f6674c8414efba0b3314ea5
sooyyoung <sooyyyoung@gmail.com>
2022-08-29 20:12:29 +0900

    Docs: useState 추가 및 form 구조 변경

--- src/components/Form/JoinForm.jsx
+++ src/components/Form/JoinForm.jsx
@@ -1,30 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import FormHeader from "./FormHeader";
 import icon from "../../assets/check-off.svg";
 
 export default function JoinForm() {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [pwCheck, setPwCheck] = useState("");
+  const [name, setName] = useState("");
+  const [phoneNum, setPhoneNum] = useState("");
+
   return (
     <JoinContainer>
       <FormHeader buyer="구매회원가입" seller="판매회원가입" />
       <form>
         <label htmlFor="id">아이디</label>
-        <input type="text" id="id" className="id" />
+        <input
+          type="text"
+          id="id"
+          className="id"
+          required
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
         <button className="idCheck" type="button">
           중복확인
         </button>
 
         <label htmlFor="pw">비밀번호</label>
-        <input type="password" id="pw" className="pw" />
+        <input
+          type="password"
+          id="pw"
+          className="pw"
+          required
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+        />
 
         <label htmlFor="pwCheck">비밀번호 재확인</label>
-        <input type="password" id="pwCheck" className="pw" />
+        <input
+          type="password"
+          id="pwCheck"
+          className="pw"
+          required
+          value={pwCheck}
+          onChange={(e) => setPwCheck(e.target.value)}
+        />
 
         <label htmlFor="name">이름</label>
-        <input type="text" id="name" className="name" />
+        <input
+          type="text"
+          id="name"
+          className="name"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
         <label htmlFor="phoneNum">휴대폰번호</label>
-        <input type="text" id="phoneNum" className="phoneNum" />
+        <input
+          type="text"
+          id="phoneNum"
+          className="phoneNum"
+          required
+          value={phoneNum}
+          onChange={(e) => setPhoneNum(e.target.value)}
+        />
         <input type="text" className="phoneNum" />
         <input type="text" className="phoneNum" />
 
@@ -32,23 +73,23 @@ export default function JoinForm() {
         <input type="text" id="email" className="email" />
         <span className="at">@</span>
         <input type="text" className="email" />
+
+        <AcceptCheck>
+          <input type="checkbox" id="acceptCheck" className="acceptCheck" />
+          <label htmlFor="acceptCheck">
+            <strong>
+              <u>이용약관</u>
+            </strong>{" "}
+            및{" "}
+            <strong>
+              <u>개인정보처리방침</u>
+            </strong>
+            에 대한 내용을 확인하였고 동의합니다.
+          </label>
+        </AcceptCheck>
+
+        <JoinBtn type="submit">가입하기</JoinBtn>
       </form>
-
-      <AcceptCheck>
-        <input type="checkbox" id="acceptCheck" />
-        <label htmlFor="acceptCheck">
-          <strong>
-            <u>이용약관</u>
-          </strong>{" "}
-          및{" "}
-          <strong>
-            <u>개인정보처리방침</u>
-          </strong>
-          에 대한 내용을 확인하였고 동의합니다.
-        </label>
-      </AcceptCheck>
-
-      <JoinBtn>가입하기</JoinBtn>
     </JoinContainer>
   );
 }
@@ -59,12 +100,12 @@ const JoinContainer = styled.section`
     border: 1px solid #c4c4c4;
     border-top: none;
     border-radius: 0 0 10px 10px;
-    label {
+    & > label {
       color: #767676;
       margin: 20px 0 10px;
       display: block;
     }
-    input {
+    & > input {
       width: 100%;
       height: 55px;
       padding-left: 15px;
