2d16751f6bb04001bdacc0c9ef46b4c15a651ec1
sooyyoung <sooyyyoung@gmail.com>
2022-09-06 15:23:59 +0900

    Docs: styled-components 파일 분리

--- src/components/Form/JoinForm.jsx
+++ src/components/Form/JoinForm.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../../api/api";
-import styled from "styled-components";
 import FormHeader from "./FormHeader";
-import checkOff from "../../assets/check-off.svg";
-import checkOn from "../../assets/check-on.svg";
+import { JoinContainer, AcceptCheck, JoinBtn, Message } from "./JoinForm.style";
 
 export default function JoinForm() {
   const navigate = useNavigate();
@@ -276,96 +274,3 @@ export default function JoinForm() {
     </JoinContainer>
   );
 }
-
-const JoinContainer = styled.section`
-  form {
-    padding: 30px 35px 35px;
-    border: 1px solid #c4c4c4;
-    border-top: none;
-    border-radius: 0 0 10px 10px;
-    & > label {
-      color: #767676;
-      margin: 20px 0 10px;
-      display: block;
-    }
-    & > input {
-      width: 100%;
-      height: 55px;
-      padding-left: 15px;
-      display: inline-block;
-      border: 1px solid #c4c4c4;
-      border-radius: 5px;
-    }
-    .id {
-      width: 345px;
-    }
-    .pw {
-      &.checkOn {
-        background: url(${checkOn}) no-repeat right 15px center;
-      }
-      &.checkOff {
-        background: url(${checkOff}) no-repeat right 15px center;
-      }
-    }
-    .idCheck {
-      width: 120px;
-      height: 55px;
-      float: right;
-      color: #fff;
-      border-radius: 5px;
-      background-color: royalblue;
-    }
-    .phoneNum {
-      width: calc((100% - 24px) / 3);
-    }
-    .phoneNum + .phoneNum {
-      margin-left: 12px;
-    }
-    .email {
-      width: calc((100% - 35px) / 2);
-    }
-    .at {
-      margin: 0 10px;
-    }
-  }
-`;
-
-const AcceptCheck = styled.section`
-  padding: 35px;
-  font-size: 16px;
-  color: #767676;
-  input {
-    width: 18px;
-    height: 18px;
-    margin-right: 12px;
-  }
-  label {
-    vertical-align: top;
-  }
-`;
-
-const JoinBtn = styled.button`
-  width: 480px;
-  height: 60px;
-  margin: 0 auto;
-  display: block;
-  border-radius: 5px;
-  &.disabled {
-    cursor: default;
-    pointer-events: none;
-    background: #c4c4c4;
-    color: #fff;
-  }
-
-  &.active {
-    background-color: #4169e1;
-    color: #fff;
-  }
-`;
-
-const Message = styled.span`
-  display: block;
-  margin-top: 10px;
-  font-size: 16px;
-  color: ${(props) => (props.isId ? "#4169E1" : "#eb5757")};
-`;
---
+++ src/components/Form/JoinForm.style.js
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+import checkOff from "../../assets/check-off.svg";
+import checkOn from "../../assets/check-on.svg";
+
+export const JoinContainer = styled.section`
+  form {
+    padding: 30px 35px 35px;
+    border: 1px solid #c4c4c4;
+    border-top: none;
+    border-radius: 0 0 10px 10px;
+    & > label {
+      color: #767676;
+      margin: 20px 0 10px;
+      display: block;
+    }
+    & > input {
+      width: 100%;
+      height: 55px;
+      padding-left: 15px;
+      display: inline-block;
+      border: 1px solid #c4c4c4;
+      border-radius: 5px;
+    }
+    .id {
+      width: 345px;
+    }
+    .pw {
+      &.checkOn {
+        background: url(${checkOn}) no-repeat right 15px center;
+      }
+      &.checkOff {
+        background: url(${checkOff}) no-repeat right 15px center;
+      }
+    }
+    .idCheck {
+      width: 120px;
+      height: 55px;
+      float: right;
+      color: #fff;
+      border-radius: 5px;
+      background-color: royalblue;
+    }
+    .phoneNum {
+      width: calc((100% - 24px) / 3);
+    }
+    .phoneNum + .phoneNum {
+      margin-left: 12px;
+    }
+    .email {
+      width: calc((100% - 35px) / 2);
+    }
+    .at {
+      margin: 0 10px;
+    }
+  }
+`;
+
+export const AcceptCheck = styled.section`
+  padding: 35px;
+  font-size: 16px;
+  color: #767676;
+  input {
+    width: 18px;
+    height: 18px;
+    margin-right: 12px;
+  }
+  label {
+    vertical-align: top;
+  }
+`;
+
+export const JoinBtn = styled.button`
+  width: 480px;
+  height: 60px;
+  margin: 0 auto;
+  display: block;
+  border-radius: 5px;
+  &.disabled {
+    cursor: default;
+    pointer-events: none;
+    background: #c4c4c4;
+    color: #fff;
+  }
+
+  &.active {
+    background-color: #4169e1;
+    color: #fff;
+  }
+`;
+
+export const Message = styled.span`
+  display: block;
+  margin-top: 10px;
+  font-size: 16px;
+  color: ${(props) => (props.isId ? "#4169E1" : "#eb5757")};
+`;
