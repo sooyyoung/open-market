c92a1f6094ae3f11c914a3d61529ce1ed992fd50
sooyyoung <sooyyyoung@gmail.com>
2022-09-06 15:29:39 +0900

    Docs: styled-components 파일 분리

---
+++ src/components/Form/LoginForm.style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.section`
+  form {
+    padding: 50px 35px 35px;
+    border: 1px solid #c4c4c4;
+    border-top: none;
+    border-radius: 0 0 10px 10px;
+    input {
+      width: 100%;
+      padding: 20px 0;
+      margin-bottom: 5px;
+      font-size: 16px;
+      color: #767676;
+      border-bottom: 1px solid #c4c4c4;
+    }
+    button.loginBtn {
+      width: 100%;
+      height: 60px;
+      margin-top: 30px;
+      border-radius: 5px;
+      font-size: 18px;
+      font-weight: bold;
+      color: #fff;
+      background-color: royalblue;
+    }
+  }
+`;
