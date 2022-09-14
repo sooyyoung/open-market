58f406143dc5fe1d8621ebc1811dcf39991b6eaa
sooyyoung <sooyyyoung@gmail.com>
2022-09-14 20:54:35 +0900

    Docs: 불필요 코드 삭제

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,6 @@
 import React from "react";
-import Prev from "../../assets/swiper-1.svg";
-import Next from "../../assets/swiper-2.svg";
 import { Container } from "./Banner.style";
 
 export default function Banner() {
-  return (
-    <Container>
-      <button>
-        <img src={Prev} alt="" />
-      </button>
-      <button>
-        <img src={Next} alt="" />
-      </button>
-    </Container>
-  );
+  return <Container></Container>;
 }
