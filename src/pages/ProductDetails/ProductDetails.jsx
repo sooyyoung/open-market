f57395abf340d0bf5868465cb593db1a932a5ce8
sooyoung <sooyyyoung@gmail.com>
2022-10-19 17:34:18 +0900

    Feat: 상품 상세 페이지 마크업

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,45 @@
 import React from "react";
+import Nav from "../../components/Nav/Nav";
+import {
+  Container,
+  Details,
+  ProductInfo,
+  AmountBtn,
+  ProductPrice,
+  Button,
+} from "./ProductDetails.style";
 
 export default function ProductDetails() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <Container>
+        <img src="https://via.placeholder.com/600" alt="" />
+        <Details>
+          <ProductInfo>
+            <span>상품 판매자</span>
+            <p>상품명</p>
+            <strong>가격</strong>원
+            <span className="delivery">택배배송 / 무료배송</span>
+          </ProductInfo>
+
+          <AmountBtn>
+            <button className="minus">-</button>
+            <span>1</span>
+            <button className="plus">+</button>
+          </AmountBtn>
+
+          <ProductPrice>
+            <p>총 상품 금액</p>
+            <span>총 수량 1개</span>
+            <span className="verticalBar">|</span>
+            <strong>17,500</strong>원
+          </ProductPrice>
+
+          <Button BuyBtn>바로 구매</Button>
+          <Button CartBtn>장바구니</Button>
+        </Details>
+      </Container>
+    </>
+  );
 }
--- src/pages/ProductDetails/ProductDetails.style.js
+++ src/pages/ProductDetails/ProductDetails.style.js
@@ -1,1 +1,107 @@
 import styled from "styled-components";
+
+export const Container = styled.section`
+  max-width: 1280px;
+  height: 600px;
+  margin: 80px auto 0;
+  display: flex;
+  img {
+    width: 600px;
+    height: 600px;
+    border: 1px solid #c4c4c4;
+  }
+`;
+
+export const Details = styled.section`
+  width: 100%;
+  height: 600px;
+  margin-left: 50px;
+`;
+
+export const ProductInfo = styled.section`
+  font-size: 18px;
+  border-bottom: 2px solid #c4c4c4;
+  span {
+    display: block;
+    margin-bottom: 15px;
+    color: #767676;
+  }
+  p {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  strong {
+    font-size: 36px;
+  }
+  .delivery {
+    margin: 138px 0 20px;
+    font-size: 16px;
+  }
+`;
+
+export const AmountBtn = styled.section`
+  width: 150px;
+  height: 50px;
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid #c4c4c4;
+  border-radius: 5px;
+  line-height: 45px;
+  overflow: hidden;
+  button {
+    width: 50px;
+    font-size: 20px;
+    &.minus {
+      border-right: 1px solid #c4c4c4;
+    }
+    &.plus {
+      border-left: 1px solid #c4c4c4;
+    }
+  }
+  span {
+    color: #000;
+  }
+`;
+
+export const ProductPrice = styled.section`
+  padding-top: 45px;
+  display: flex;
+  border-top: 2px solid #c4c4c4;
+  font-size: 18px;
+  p {
+    font-weight: bold;
+    margin-right: auto;
+  }
+  span {
+    color: #767676;
+  }
+  .verticalBar {
+    margin: 0 12px;
+    color: #c4c4c4;
+  }
+  strong {
+    font-size: 36px;
+    color: #007aff;
+  }
+`;
+
+export const Button = styled.button`
+  height: 60px;
+  font-size: 18px;
+  font-weight: bold;
+  border-radius: 5px;
+  ${(props) => {
+    if (props.BuyBtn) {
+      return `width: 415px; 
+      background-color: #007aff;
+      color: #fff;`;
+    }
+    if (props.CartBtn) {
+      return `width: 200px;
+      float: right;
+      background-color: #767676;
+      color: #fff;`;
+    }
+  }}
+`;
