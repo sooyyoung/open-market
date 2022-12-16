53c545c7b86aa3a246191035ec78f85d044c5ae1
sooyoung <sooyyyoung@gmail.com>
2022-12-16 10:41:21 +0900

    Docs: headers 추가

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://openmarket.weniv.co.kr",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
