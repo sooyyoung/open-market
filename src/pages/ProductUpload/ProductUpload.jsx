006e0f5bdcbe01930a464c1bb7568a6659d2be44
sooyyoung <sooyyyoung@gmail.com>
2023-03-31 17:06:14 +0900

    Feat: ProductUpload 페이지 마크업

--- src/pages/ProductUpload/ProductUpload.jsx
+++ src/pages/ProductUpload/ProductUpload.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import Nav from "../../components/Nav/Nav";
+import { Main, Dashboard, Notice, Container } from "./ProductUpload.style";
 
 export default function ProductUpload() {
-  return <></>;
+  return (
+  <>
+    <Nav />
+    <Main>
+        <strong>상품 등록</strong>
+        <Dashboard>
+            <Notice>
+                <p>* 상품 등록 주의사항</p>
+                <div>- 너무 귀여운 사진은 심장이 아파올 수 있습니다.</div>
+            </Notice>
+            <Container>
+                <div>
+                    <div className="imgBox">
+                        <p>상품 이미지</p>
+                        <img src="" alt="" />
+                    </div>
+                    <div className="infoBox">
+                        <p>상품명</p>
+                        <input type="text" style={{ "width" : "100%"}} />
+                        <p>판매가</p>
+                        <input type="text" /><span>원</span>
+                        <p>배송방법</p> 
+                        <button>택배, 소포, 등기</button> 
+                        <button>직접배송(화물배달)</button>
+                        <p>기본 배송비</p>
+                        <input type="text" /><span>원</span>
+                        <p>재고</p>
+                        <input type="text" /><span>개</span>
+                    </div>
+                </div>
+                <div className="detailBox">
+                    <p>상품 상세 정보</p>
+                    <div className="edit">
+                        <textarea></textarea>
+                    </div>
+                </div>
+                <div className="btn">
+                    <button>취소</button>
+                    <button>저장하기</button>
+                </div>
+            </Container>
+        </Dashboard>
+    </Main>
+  </>
+  )
 }
--- src/pages/ProductUpload/ProductUpload.style.js
+++ src/pages/ProductUpload/ProductUpload.style.js
@@ -1,1 +1,106 @@
 import styled from "styled-components";
+import uploadImg from "../../assets/icon-img.svg";
+
+export const Main = styled.section`
+  padding: 45px 100px;
+  strong {
+    font-size: 36px;
+  }
+`;
+
+export const Dashboard = styled.section`
+  width: 100%;
+  margin-top: 45px;
+  display: flex;
+`;
+
+export const Notice = styled.div`
+  margin-right: 30px;
+  p {
+    font-size: 16px;
+    font-weight: 500;
+    color: #EB5757;
+    margin-bottom: 10px;
+  }
+  div {
+    width: 280px;
+    padding: 20px;
+    border-radius: 5px;
+    background-color: #FFEFE8;
+    font-size: 14px;
+  }
+`;
+
+export const Container = styled.section`
+  width: 100%;
+  > div {
+    display: flex;
+    margin-bottom: 40px;
+  }
+  p {
+    color: #767676;
+    margin-bottom: 6px;
+  }
+  span {
+    margin-left: 6px;
+    color: #767676;
+  }
+  .imgBox {
+    width: 450px;
+    height: 450px;
+    margin-right: 30px;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        background: url(${uploadImg}) no-repeat center #C4C4C4;
+    }
+  }
+  .infoBox {
+    width: calc(100% - 455px);
+  }
+  input {
+    width: 220px;
+    height: 54px;
+    padding: 15px;
+    border: 1px solid #C4C4C4;
+    border-radius: 5px;
+    margin-bottom: 15px;
+  }
+  button {
+    width: 200px;
+    height: 54px;
+    border: 1px solid #C4C4C4;
+    border-radius: 5px;
+    margin: 0 10px 15px 0;
+  }
+  .detailBox {
+    width: 100%;
+    display: block;
+    .edit {
+        height: 200px;
+        padding: 10px;
+        background: #F2F2F2;
+        border: 1px solid #C4C4C4;
+        border-radius: 5px;
+    }
+    textarea {
+        width: 100%;
+        height: 100%;
+        background: #F2F2F2;
+        border: none;
+        resize: none;
+        font-size: 16px;
+    }
+  }
+  .btn {
+    float: right;
+    button {
+        &:last-child {
+            background-color: royalblue;
+            color: #fff;
+            margin-right: 0;
+        }
+    }
+  }
+`
