5436a6a9ca9dda1b0834d2c65873d71d2e2ff08e
sooyyoung <sooyyyoung@gmail.com>
2023-04-04 17:03:29 +0900

    Feat: 상품 수정 기능 추가

--- src/components/Products/SellerProductList/SellerProductList.jsx
+++ src/components/Products/SellerProductList/SellerProductList.jsx
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
 import API from "../../../api/api";
-import { Button } from "./SellerProductList.style";
+import { List, Button } from "./SellerProductList.style";
 
-export default function SellerProductList({ item }) {
+export default function SellerProductList(props) {
+    const { item, elementIndex, isSelect, modifyProduct } = props;
+    const [image, setImage] = useState("");
+    const [previewImg, setPreviewImg] = useState(item.image);
+    const [productName, setProductName] = useState(item.product_name);
+    const [productInfo, setProductInfo] = useState(item.product_info);
+    const [price, setPrice] = useState(item.price);
+    const [stock, setStock] = useState(item.stock);
+    const regExp = /^[0-9]+$/;
+
+    const imgUpload = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setImage(file);
+            setPreviewImg(reader.result);
+        };
+    }
+
     const deleteProduct = async (e) => {
         const productId = e.target.id;
         if (!window.confirm("상품을 삭제하시겠습니까?")) return
@@ -12,31 +32,115 @@ export default function SellerProductList({ item }) {
                 },
             });
             alert("상품이 삭제되었습니다.");
+            window.location.reload();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const modifySuccess = async () => {
+        try {
+            const res = await API.put(`/products/${item.product_id}/`, {
+                product_name: productName,
+                price: price,
+                image: image,
+                shipping_method: item.shipping_method,
+                shipping_fee: item.shipping_fee,
+                stock: stock,
+                product_info: productInfo
+            }, {
+                headers: {
+                    Authorization: window.localStorage.getItem("token"),
+                    "Content-Type": "multipart/form-data",
+                }
+            });
+            if (res.status === 200) {
+                modifyProduct(false);
+            }
         } catch (error) {
             console.error(error);
         }
     }
      
     return (
-        <tr>
+        <List className={isSelect ? "select" : ""}>
             <td className="product">
-                <img src={item.image} alt={item.product_name} />
-                <div>
-                    <p>{item.product_name}</p>
-                    <span>재고: {item.stock}개</span>
-                </div>
+                <label htmlFor="imgFile">
+                    <img src={previewImg} alt={item.product_name} />
+                </label>
+                <input 
+                    type="file"
+                    id="imgFile"
+                    accept="image/*"
+                    onChange={(e) => imgUpload(e)}
+                    style={{ "display" : "none"}}
+                />
+                {!isSelect ?
+                    <div>
+                        <p>{item.product_name}</p>
+                        <span>재고: {item.stock}개</span>
+                    </div> 
+                : 
+                    <div>
+                        <input 
+                            type="text" 
+                            value={productName}
+                            onChange={(e) => {setProductName(e.target.value)}}
+                            className="productName"
+                        />
+                        <textarea 
+                            value={productInfo} 
+                            onChange={(e) => {setProductInfo(e.target.value)}}
+                        ></textarea>
+                        재고: 
+                        <input 
+                            type="text"
+                            value={stock}
+                            onChange={(e) => {
+                                if (regExp.test(e.target.value)) {
+                                    setStock(e.target.value);
+                                }
+                            }}
+                            className="stock"
+                        />
+                    </div>
+                }
             </td>
-            <td>{item.price}원</td>
             <td>
-                <Button modify>수정</Button>
+                {!isSelect ?
+                    <>{item.price}원</>
+                :
+                    <>
+                        <input 
+                            type="text" 
+                            value={price}
+                            onChange={(e) => {
+                                if (regExp.test(e.target.value)) {
+                                    setPrice(e.target.value);
+                                }
+                            }}
+                            className="price"
+                        /> 원
+                    </>
+                }
             </td>
             <td>
-                <Button 
-                    onClick={deleteProduct} 
-                    id={item.product_id} 
-                    delete
-                >삭제</Button>
+                {!isSelect ? 
+                    <Button onClick={() => {modifyProduct(elementIndex)}}>수정</Button>
+                : 
+                    <Button onClick={modifySuccess}>완료</Button>
+                }
             </td>
-        </tr>
+            <td>
+                {!isSelect ?
+                    <Button 
+                        onClick={deleteProduct} 
+                        id={item.product_id} 
+                        delete
+                    >삭제</Button>
+                : <Button onClick={() => modifyProduct(false)} delete>취소</Button>
+                }
+            </td>
+        </List>
     )
 }
--- src/components/Products/SellerProductList/SellerProductList.style.js
+++ src/components/Products/SellerProductList/SellerProductList.style.js
@@ -1,18 +1,75 @@
 import styled from "styled-components";
 
+export const List = styled.tr`
+  &.select {
+    background-color: #f2f2f2;
+  }
+  &:last-child {
+    border-bottom: none;
+  }
+  td.product {
+    text-align: start;
+    display: flex;
+    align-items: center;
+    vertical-align: middle;
+    img {
+        width: 70px;
+        height: 70px;
+        padding: 15px 30px;
+        border-radius: 50%;
+    }
+    p {
+        font-size: 18px;
+        margin-bottom: 10px;
+    }
+    span {
+        font-size: 16px;
+        color: #767676;
+    }
+  }
+  div {
+    width: 90%;
+  }
+  input {
+    display: block;
+    padding: 5px;
+    margin: 15px 0;
+    background: #fff;
+    border-bottom: 1px solid #767676;
+    &.productName {
+        width: 100%;
+    }
+    &.stock, &.price {
+        display: inline-block;
+        width: 30%;
+    }
+  }
+  textarea {
+    width: 100%;
+    margin-top: 10px;
+    padding: 5px;
+    border: none;
+    border-bottom: 1px solid #767676;
+    resize: none;
+    display: block;
+   }
+`
+
 export const Button = styled.button`
   width: 80px;
   height: 40px;
   border-radius: 5px;
   ${(props) => {
-    if (props.modify) {
+    if (props.delete) {
+      return `
+        background-color: #fff; 
+        border: 1px solid #767676;
+      `;
+    } else {
       return `
         background-color: royalblue; 
         color: #fff
       `;
     }
-    if (props.delete) {
-      return `border: 1px solid #767676;`;
-    }
-  }};
+  }}
 `;
