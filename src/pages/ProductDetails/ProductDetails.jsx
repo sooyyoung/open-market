6e0edcd82b7e436503676ff5f3deadeaa3c50896
sooyyoung <sooyyyoung@gmail.com>
2023-04-05 16:29:08 +0900

    Docs: useState 정리 & 코드 정리

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -13,15 +13,9 @@ import {
 } from "./ProductDetails.style";
 
 export default function ProductDetails() {
-  const [productImage, setProductImage] = useState();
-  const [store, setStore] = useState();
-  const [productName, setProductName] = useState();
-  const [productInfo, setProductInfo] = useState();
-  const [shippingFee, setShippingFee] = useState();
-  const [price, setPrice] = useState(); 
+  const [product, setProduct] = useState([]);
   const [quantity, setQuantity] = useState(1);
-  const totalPrice = price * quantity;
-  const [check, setCheck] = useState(true);
+  const totalPrice = product.price * quantity;
   const [soldout, setSoldout] = useState(true);
   let [tab, setTab] = useState(0);
 
@@ -35,12 +29,7 @@ export default function ProductDetails() {
   const getProduct = async () => {
     try {
       const res = await API.get(`/products/${productId}/`);
-      setProductImage(res.data.image);
-      setStore(res.data.store_name);
-      setProductName(res.data.product_name);
-      setProductInfo(res.data.product_info);
-      setShippingFee(res.data.shipping_fee);
-      setPrice(res.data.price); 
+      setProduct(res.data);
       if (res.data.stock === 0) {
         setSoldout(false);
       }
@@ -54,7 +43,7 @@ export default function ProductDetails() {
         const res = await API.post("/cart/", {
             product_id: productId, 
             quantity: quantity,
-            check : check
+            check : soldout
         }, {
             headers: {
                 Authorization: window.localStorage.getItem("token"),
@@ -68,15 +57,6 @@ export default function ProductDetails() {
     }
   }
 
-  // +, - 버튼
-  const quantityMinus = () => {
-    setQuantity((current) => (current > 1 ? current - 1 : 1));
-  };
-
-  const quantityPlus = () => {
-    setQuantity(quantity + 1);
-  };
-
   // tab
   const changeTab = (index) => {
     setTab(index);
@@ -84,7 +64,7 @@ export default function ProductDetails() {
 
   // tab list
   const tabList = {
-    0: productInfo,
+    0: product.product_info,
   };
 
   return (
@@ -92,20 +72,20 @@ export default function ProductDetails() {
       <Nav />
       <Container>
         <div>
-          <img src={productImage} alt="" />
+          <img src={product.image} alt="" />
         </div>
         <Details>
           <ProductInfo>
-            <span>{store}</span>
-            <p>{productName}</p>
-            <strong>{Number(price).toLocaleString()}</strong>원
-            <span className="delivery">택배배송 / {shippingFee}</span>
+            <span>{product.store_name}</span>
+            <p>{product.product_name}</p>
+            <strong>{Number(product.price).toLocaleString()}</strong>원
+            <span className="delivery">택배배송 / {product.shipping_fee}</span>
           </ProductInfo>
 
           <AmountBtn>
-            <button className="minus" onClick={quantityMinus}>-</button>
+            <button className="minus" onClick={() => setQuantity((current) => (current > 1 ? current - 1 : 1))}>-</button>
             <span>{quantity}</span>
-            <button className="plus" onClick={quantityPlus}>+</button>
+            <button className="plus" onClick={() => setQuantity(quantity + 1)}>+</button>
           </AmountBtn>
 
           <ProductPrice>
@@ -131,23 +111,19 @@ export default function ProductDetails() {
           <li
             onClick={() => {changeTab(0)}}
             className={tab === 0 ? "active" : null}
-          > 상품 설명
-          </li>
+          >상품 설명</li>
           <li
             onClick={() => {changeTab(1)}}
             className={tab === 1 ? "active" : null}
-          > 리뷰
-          </li>
+          >리뷰</li>
           <li
             onClick={() => {changeTab(2)}}
             className={tab === 2 ? "active" : null}
-          > Q&A
-          </li>
+          >Q&A</li>
           <li
             onClick={() => {changeTab(3)}}
             className={tab === 3 ? "active" : null}
-          > 반품/교환정보
-          </li>
+          >반품/교환정보</li>
         </ul>
         <div>{tabList[tab]}</div>
       </ProductDescription>
